a7d2526fdec6fa9086b8ef800d9c7c4eae004be9
Isaikin Pavel <pawel.isaykin@gmail.com>
2021-06-06 23:14:59 +0300

    Listener separated

---
+++ src/dcgui/ListenButton.java
@@ -0,0 +1,36 @@
+package dcgui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+public class ListenButton implements ActionListener {
+    JButton button;
+    JTextField isd;
+    JTextField isD;
+    JTextField isAB;
+    JTextField tfRes;
+
+    DecimalFormat df = new DecimalFormat("###.##");
+
+    public ListenButton(JButton button, JTextField isd, JTextField isD, JTextField isAB, JTextField tfRes){
+        this.button = button;
+        this.isd = isd;
+        this.isD = isD;
+        this.isAB = isAB;
+        this.tfRes =tfRes;
+    }
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        double d = Double.parseDouble(isd.getText().replaceAll(",", "."));
+        double D = Double.parseDouble(isD.getText().replaceAll(",", "."));
+        double ab = Double.parseDouble(isAB.getText().replaceAll(",", "."));
+        tfRes.setText(String.valueOf(df.format(D * ((d + ab) / (2 + ab)))));
+
+    }
+}
--- src/dcgui/SwingGUI.java
+++ src/dcgui/SwingGUI.java
@@ -4,11 +4,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.DecimalFormat;
 
 public class SwingGUI extends JFrame implements ActionListener {
 
+    private JButton button = new JButton("EQD2 =");
+    private JTextField isd = new JTextField();
+    private JTextField isD = new JTextField();
+    private JTextField isAB = new JTextField();
+    private JTextField tfRes = new JTextField(10);
+
     public SwingGUI() {
+
+        ListenButton listen = new ListenButton(button, isd, isD, isAB, tfRes);
+        button.addActionListener(listen);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500, 200);
         setLocation(700, 300);
@@ -29,17 +38,14 @@ public class SwingGUI extends JFrame implements ActionListener {
         panelDown.setBackground(Color.lightGray);
         add(panelDown, BorderLayout.SOUTH);
 
-        JTextField isd = new JTextField();
         isd.setHorizontalAlignment(SwingConstants.CENTER);
         isd.setPreferredSize(new Dimension(150, 20));
         isd.setInputVerifier(new MyInputVerifier());
 
-        JTextField isD = new JTextField();
         isD.setHorizontalAlignment(SwingConstants.CENTER);
         isD.setPreferredSize(new Dimension(150, 20));
         isD.setInputVerifier(new MyInputVerifier());
 
-        JTextField isAB = new JTextField();
         isAB.setHorizontalAlignment(SwingConstants.CENTER);
         isAB.setPreferredSize(new Dimension(150, 20));
         isAB.setInputVerifier(new MyInputVerifier());
@@ -62,32 +68,8 @@ public class SwingGUI extends JFrame implements ActionListener {
         alfaBeta.setVerticalAlignment(SwingConstants.CENTER);
         alfaBeta.setPreferredSize(new Dimension(150, 20));
 
-        JTextField tfRes = new JTextField(10);
         tfRes.setHorizontalAlignment(SwingConstants.CENTER);
         tfRes.setEditable(false);
-
-        DecimalFormat df = new DecimalFormat("###.##");
-
-        JButton button = new JButton("EQD2 =");
-        button.addActionListener(e -> {if (e.getSource() == button){
-            String isdS = isd.getText();
-            isdS = isdS.replaceAll(",",".");
-            double d = Double.parseDouble(isdS);
-            System.out.println(d);
-            String isDS = isD.getText();
-            isDS = isDS.replaceAll(",",".");
-            double D = Double.parseDouble(isDS);
-            System.out.println(D);
-            String isABS = isAB.getText();
-            isABS = isABS.replaceAll(",",".");
-            double ab = Double.parseDouble(isABS);
-            System.out.println(ab);
-            System.out.printf("%f * ((%f + %f)/(2 + %f))", D, d, ab, ab);
-            double eqdTwo = D * ((d + ab) / (2 + ab));
-            System.out.println(eqdTwo);
-            tfRes.setText(String.valueOf(df.format(eqdTwo)));
-        }
-    });
 
         panelUp.add(isd, BorderLayout.WEST);
         panelUp.add(isD, BorderLayout.CENTER);
@@ -103,35 +85,6 @@ public class SwingGUI extends JFrame implements ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e){}
+    public void actionPerformed(ActionEvent e) {
+    }
 }
-
-//class Terminal {
-//        public static void main(String[] args) {
-//            EventQueue.invokeLater(() -> {
-//                try {
-//                    new Core();
-//                } catch (Exception e) {
-//                    e.printStackTrace();}});}}
-
-//class MyInputVerifier extends InputVerifier {
-//
-//        @Override
-//        public boolean verify(JComponent input) {
-//            String doubleNum = ((JTextField) input).getText();
-//            try {
-//                doubleNum = doubleNum.replaceAll(",",".");
-//                Double.parseDouble(doubleNum);
-//                return true;
-//            } catch (Exception e) {
-//                Pop.infoBox("введите число", "ввода");
-//                return false;
-//            }
-//        }
-//        public static class Pop
-//        {
-//            public static void infoBox(String infoMessage, String titleBar)
-//            {
-//                JOptionPane.showMessageDialog(null, infoMessage, "Ошибка " + titleBar, JOptionPane.INFORMATION_MESSAGE);
-//            }
-//        }
