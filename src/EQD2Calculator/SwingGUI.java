5533d5a1d069b197dd61c986ae03e3f16cc33806
Isaikin Pavel <pawel.isaykin@gmail.com>
2021-06-07 20:44:24 +0300

    GUI Finalize

--- src/EQD2Calculator/SwingGUI.java
+++ src/EQD2Calculator/SwingGUI.java
@@ -1,7 +1,6 @@
 package EQD2Calculator;
 
 import javax.swing.*;
-import javax.swing.plaf.basic.BasicComboBoxRenderer;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,74 +11,85 @@ public class SwingGUI extends JFrame implements ActionListener {
     private JTextField isd = new JTextField();
     private JTextField isD = new JTextField();
     private JTextField isAB = new JTextField();
-    private JTextField tfRes = new JTextField(10);
-    String[] ABs = {"Легкое", "Простата", "Кость", "Печень", "Мозг"};
+    private JTextField tfRes = new JTextField(6);
+    String[] ABs = {"Выберите ткань", "Легкое", "Простата", "Кость", "Печень", "Мозг"};
     private JComboBox ABChoice = new JComboBox(ABs);
 
     public SwingGUI() {
 
         ImageIcon img = new ImageIcon(getClass().getResource("/EQD2Calculator/radiation.png"));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 200);
-        setLocation(700, 300);
-        setTitle("EQD2 Калькулятор");
+        setSize(260, 420);
+        setLocation(830, 190);
+        setResizable(false);
+        setTitle("EQD2");
         setVisible(true);
         setIconImage(img.getImage());
 
+        JPanel mainP = new JPanel();
+        GridLayout gl = new GridLayout(5,1);
+        mainP.setLayout(gl);
+        add(mainP);
+
         JPanel panelUp = new JPanel();
         panelUp.setBackground(Color.lightGray);
-        add(panelUp, BorderLayout.NORTH);
-
+        mainP.add(panelUp);
         JPanel panelMid = new JPanel();
         panelMid.setBackground(Color.lightGray);
-        add(panelMid, BorderLayout.CENTER);
-
+        mainP.add(panelMid);
+        JPanel panel4ab = new JPanel();
+        panel4ab.setBackground(Color.lightGray);
+        mainP.add(panel4ab);
+        JPanel panelPH = new JPanel();
+        panelPH.setBackground(Color.lightGray);
+        mainP.add(panelPH);
         JPanel panelDown = new JPanel();
         panelDown.setBackground(Color.lightGray);
-        add(panelDown, BorderLayout.SOUTH);
+        mainP.add(panelDown);
 
         ABChoice.setEditable(false);
-        ABChoice.setMaximumRowCount(6);
+        ABChoice.setMaximumRowCount(10);
         ABChoice.setSelectedIndex(0);
-
+        ABChoice.setPreferredSize(new Dimension(150, 20));
         isd.setHorizontalAlignment(SwingConstants.CENTER);
         isd.setPreferredSize(new Dimension(150, 20));
-
         isD.setHorizontalAlignment(SwingConstants.CENTER);
         isD.setPreferredSize(new Dimension(150, 20));
-
         isAB.setHorizontalAlignment(SwingConstants.CENTER);
         isAB.setPreferredSize(new Dimension(150, 20));
+        tfRes.setEditable(false);
 
         JLabel dpf = new JLabel("Разовая доза");
         dpf.setBackground(Color.lightGray);
         dpf.setHorizontalAlignment(SwingConstants.CENTER);
         dpf.setVerticalAlignment(SwingConstants.CENTER);
         dpf.setPreferredSize(new Dimension(150, 20));
-
         JLabel dpc = new JLabel("Суммарная доза");
         dpc.setBackground(Color.lightGray);
         dpc.setHorizontalAlignment(SwingConstants.CENTER);
         dpc.setVerticalAlignment(SwingConstants.CENTER);
         dpc.setPreferredSize(new Dimension(150, 20));
-
         JLabel alfaBeta = new JLabel("α/β ткани");
         alfaBeta.setBackground(Color.lightGray);
         alfaBeta.setHorizontalAlignment(SwingConstants.CENTER);
         alfaBeta.setVerticalAlignment(SwingConstants.CENTER);
         alfaBeta.setPreferredSize(new Dimension(150, 20));
+        JLabel placeHolder = new JLabel("       ");
+        placeHolder.setBackground(Color.lightGray);
+        placeHolder.setHorizontalAlignment(SwingConstants.CENTER);
+        placeHolder.setVerticalAlignment(SwingConstants.CENTER);
+        placeHolder.setPreferredSize(new Dimension(150, 20));
 
-        tfRes.setEditable(false);
-
-        panelUp.add(isd, BorderLayout.WEST);
-        panelUp.add(isD, BorderLayout.CENTER);
-        panelUp.add(isAB, BorderLayout.EAST);
-        panelMid.add(dpf, BorderLayout.WEST);
-        panelMid.add(dpc, BorderLayout.CENTER);
-        panelMid.add(alfaBeta, BorderLayout.EAST);
-        panelDown.add(ABChoice, BorderLayout.WEST);
-        panelDown.add(button, BorderLayout.EAST);
-        panelDown.add(tfRes, BorderLayout.CENTER);
+        panelUp.add(dpf);
+        panelUp.add(isd);
+        panelMid.add(dpc);
+        panelMid.add(isD);
+        panel4ab.add(alfaBeta);
+        panel4ab.add(isAB);
+        panel4ab.add(ABChoice);
+        panelPH.add(placeHolder);
+        panelDown.add(button);
+        panelDown.add(tfRes);
 
         ListenButton listen = new ListenButton(button, isd, isD, isAB, tfRes);
         ListenABChoice listen1 = new ListenABChoice(ABChoice, isAB);
